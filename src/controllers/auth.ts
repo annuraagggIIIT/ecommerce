0d73177141ab06c2b72352d93dd9e439f05c3b48
annuraagggiiit <anuragkhanna@iiitmanipur.ac.in>
2026-01-27 01:24:23 +0530

    added login functionality with JWT

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,14 +1,32 @@
 import type { Request, Response } from "express";
 import { prismaClient } from "../index.ts";
-import {hashSync} from "bcrypt";
+import { hashSync, compareSync } from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets.ts";
 export const signup = async (req: Request, res: Response) => {
-    const {email, password , name} = req.body;
+    const { email, password, name } = req.body;
     let user = await prismaClient.user.findFirst({ where: { email } });
     if (user) {
         return res.status(400).json({ message: "User already exists" });
     }
     user = await prismaClient.user.create({
-        data:{name, email, password: hashSync(password, 10)}
+        data: { name, email, password: hashSync(password, 10) }
     });
-res.json(user)
+    res.json(user)
+}
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    let user = await prismaClient.user.findFirst({ where: { email } });
+    if (!user) {
+        return res.status(400).json({ message: "User does not exists" });
+    }
+    if (!(compareSync(password, user.password))) {
+
+        throw new Error("Invalid password")
+    }
+    const token =jwt.sign({
+        userId: user.id,
+    },JWT_SECRET)
+
+    res.json({user,token})
 }
